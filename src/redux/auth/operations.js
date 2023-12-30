e9216d6f2ab83b8e7aec08d472b950c3a0edca3b
artem-yaremchuk <119439325+artem-yaremchuk@users.noreply.github.com>
2023-12-30 19:16:44 +0200

    Feat: Add notifications for Auth

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
 axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
@@ -17,6 +18,7 @@ export const register = createAsyncThunk(
     try {
       const res = await axios.post("/users/signup", credentials);
       setAuthHeader(res.data.token);
+      toast.success(`${credentials.name}, welcome to the Phonebook`);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -30,6 +32,7 @@ export const logIn = createAsyncThunk(
     try {
       const res = await axios.post("/users/login", credentials);
       setAuthHeader(res.data.token);
+      toast.success("Welcome back");
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
